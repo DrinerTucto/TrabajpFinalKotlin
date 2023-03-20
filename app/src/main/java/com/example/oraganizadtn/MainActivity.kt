31f9fb6bdfe14a5213926783b30aaf240c1c15de
DrinerTucto <1304745@senati.pe>
2023-03-20 18:38:55 -0500

    first commit

---
+++ app/src/main/java/com/example/oraganizadtn/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.oraganizadtn
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val r1 = findViewById<RadioButton>(R.id.radioButton)
+        val r2 = findViewById<RadioButton>(R.id.radioButton2)
+        val r3 = findViewById<RadioButton>(R.id.radioButton3)
+
+        val btn = findViewById<Button>(R.id.button2)
+
+        btn.setOnClickListener {
+            if (r1.isChecked) {
+                validarhm("hombre")
+
+            }
+            if (r2.isChecked) {
+                validarhm("mujer")
+            }
+            if(r3.isChecked){
+                validarhm("otro")
+
+            }
+        }
+
+    }
+
+    fun validarhm(validar: String) {
+        val intent = Intent(this, loginActivity2::class.java)
+        var genero = validar
+        intent.putExtra("hombre", genero)
+        startActivity(intent)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/oraganizadtn/loginActivity2.kt
@@ -0,0 +1,38 @@
+package com.example.oraganizadtn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+
+
+
+class loginActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+        val image = findViewById<ImageView>(R.id.image)
+        val image2 = findViewById<ImageView>(R.id.image1)
+        val image3 = findViewById<ImageView>(R.id.otro)
+
+
+        val name = intent.extras?.getString("hombre").orEmpty()
+
+        if (name == "hombre") {
+          image2.visibility=View.VISIBLE
+
+
+        }
+        if (name == "mujer") {
+            image.visibility=View.VISIBLE
+
+
+        }
+        if (name=="otro"){
+            image3.visibility=View.VISIBLE
+
+        }
+
+    }
+}
